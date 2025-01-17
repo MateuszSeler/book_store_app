b0eb000e3cf67e7c3571211f090c73420d515a25
Mateusz Seler <mateusz.seler@gmail.com>
2025-01-17 12:21:03 +0100

    TENTH STEP: 1. Category entity, dto, controller and mapper repository added 2. Book entity, dto, repository, specification provider and mapper developed 3. Database filed by liquibase with new data 4. findAllByCategoryId implemented

--- src/main/java/app/dto/book/BookCreateRequestDto.java
+++ src/main/java/app/dto/book/BookCreateRequestDto.java
@@ -5,10 +5,11 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
+import java.util.Set;
 import lombok.Data;
 
 @Data
-public class CreateBookRequestDto {
+public class BookCreateRequestDto {
     @NotNull
     @Size(min = 1)
     private String title;
@@ -21,4 +22,6 @@ public class CreateBookRequestDto {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    private Set<Long> categoriesIds;
+
 }
