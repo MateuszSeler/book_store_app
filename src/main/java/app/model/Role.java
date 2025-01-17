3b2f402e484220baa3a9a561f94fda851163590c
Mateusz Seler <mateusz.seler@gmail.com>
2025-01-17 12:21:03 +0100

    TENTH STEP: 1. Category entity, dto, controller and mapper repository added 2. Book entity, dto, repository, specification provider and mapper developed 3. Database filed by liquibase with new data 4. findAllByCategoryId implemented

--- src/main/java/app/model/Role.java
+++ src/main/java/app/model/Role.java
@@ -11,23 +11,27 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Data
 @SQLDelete(sql = "UPDATE books SET is_Deleted = true WHERE id = ?")
 @SQLRestriction(value = "is_Deleted = false")
 @Table(name = "roles")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Enumerated(EnumType.STRING)
     private RoleName roleType;
-    @Column(nullable = false)
-    private boolean isDeleted = false;
 
     enum RoleName {
         ROLE_ADMIN,
         ROLE_USER;
+    }
+
+    @Override
+    public String getAuthority() {
+        return roleType.toString();
     }
 }
